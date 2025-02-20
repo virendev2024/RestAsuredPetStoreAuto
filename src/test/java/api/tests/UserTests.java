a544354ec3030e29c586b43af92a04d050b250ab
virender singh <virensingh2022@gmail.com>
2025-02-20 10:50:08 +0000

    added all the test cases

--- src/test/java/api/tests/UserTests.java
+++ src/test/java/api/tests/UserTests.java
@@ -47,13 +47,44 @@ public class UserTests {
     }
 
     // Uncomment and implement this method when needed
-    /*
+
     @Test(priority = 2)
-    public void getUserName() {
+    public void testGetUserByName() {
         useRelaxedHTTPSValidation();
         Response response = UserEndPoints.readUser(this.userPayload.getUserName());
         response.then().log().all();
         Assert.assertEquals(response.getStatusCode(), 200);
     }
-    */
+
+
+    @Test(priority = 3)
+    public void testUpdateUserByName()
+    {
+        //update data using payload
+        // we are not updating the username because we are doing operations using 'username' here
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+        userPayload.setEmail(faker.internet().safeEmailAddress());
+
+
+        Response response = UserEndPoints.updateUser(this.userPayload.getUserName(),userPayload);
+        // this.userPayload.getUserName() : existing username
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(),200);
+
+        //checking data after update
+        Response responseAfterUpdate = UserEndPoints.readUser(this.userPayload.getUserName());
+        Assert.assertEquals(responseAfterUpdate.getStatusCode(),200);
+
+
+    }
+
+    @Test(priority = 4)
+    public void testDeleteUserByName()
+    {
+
+        Response response = UserEndPoints.deleteUser(this.userPayload.getUserName());
+        Assert.assertEquals(response.getStatusCode(),200);
+
+    }
 }
